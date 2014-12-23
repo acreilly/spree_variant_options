5231f607ff82695b4580ac3f290fa090eab2a800
acreilly <acreilly3@gmail.com>
2014-12-23 10:22:33 -0500

    fix variant problem

--- app/controllers/spree/admin/variants_controller_decorator.rb
+++ app/controllers/spree/admin/variants_controller_decorator.rb
@@ -6,13 +6,12 @@ Spree::Admin::VariantsController.class_eval do
     @deleted = (params.key?(:deleted) && params[:deleted] == "on") ? "checked" : ""
 
     if @deleted.blank?
-      @collection ||= super
+      @collection ||= parent.variants_including_master
     else
-      @collection ||= Variant.only_deleted.where(:product_id => parent.id)
+      @collection ||= Spree::Variant.only_deleted.where(:product_id => parent.id)
     end
     @collection
   end
-
   def find_resource
     if parent_data.present?
       parent.variants_including_master.find(params[:id])
